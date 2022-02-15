fc2499e6def191a53373687c381103bdfb7694e2
alialkirzam@hotmail.com <alialkirzam@hotmail.com>
2022-02-15 15:04:37 +0000

    week4

--- AliExercise_2.py
+++ AliExercise_2.py
@@ -3,29 +3,37 @@ import csv
 #from tkinter.tix import COLUMN, ROW
 csv_file=csv.reader(open('C:/Users/alialkirzam/OneDrive - Liverpool John Moores University/Desktop/computer scince workshop/dataset/rotten_tomatoes_movies.csv','r'))
 print(csv_file)
+import re
 
 def searchByMovieTitle():
     movie_title=str(input('Enter Movie Title\n'))
-    csv_file=csv.reader(open('C:/Users/alialkirzam/OneDrive - Liverpool John Moores University/Desktop/computer scince workshop/dataset/rotten_tomatoes_movies.csv','r'))
-    for row in csv_file:
-        if movie_title in row[1]:
-            print(row[5])
+    #csv_file=csv.reader(open('C:/Users/alialkirzam/OneDrive - Liverpool John Moores University/Desktop/computer scince workshop/dataset/rotten_tomatoes_movies.csv','r'),)
+    #for row in csv_file:
+    #   if movie_title in row[0]:
+    #        print(row[3])
+    datafile =  open('C:/Users/alialkirzam/OneDrive - Liverpool John Moores University/Desktop/computer scince workshop/dataset/rotten_tomatoes_movies.csv','r', encoding="utf8")
+    for line in datafile:
+        linep = re.split(r',(?=")', line)
+        print(linep)
+    datafile.close()
+
+
 
 def searchByGenres():
     genres=str(input('Enter Genress to Show Actors and run time\n'))
     csv_file=csv.reader(open('C:/Users/alialkirzam/OneDrive - Liverpool John Moores University/Desktop/computer scince workshop/dataset/rotten_tomatoes_movies.csv','r'))
 
     for row in csv_file:
-        if genres in row[5]:
-            print(row[8:11])
+        if genres in row[2]:
+            print(row[8])
 
 def searchByDirector():
     director=str(input('Enter Director Name to show the Genres \n'))
     csv_file=csv.reader(open('C:/Users/alialkirzam/OneDrive - Liverpool John Moores University/Desktop/computer scince workshop/dataset/rotten_tomatoes_movies.csv','r'))
 
     for row in csv_file:
-        if director in row [6]:
-            print (row [5])
+        if director in row [4]:
+            print (row [3])
 
 
 print('Enter 1 to search by Movie title')
